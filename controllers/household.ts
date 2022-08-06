c69f6754c0a5ef95ce7daa3e4f77d602b8b58112
Loh Chun Mun <chunmun_loh@live.com>
2022-08-06 17:02:07 +0800

    get all household records from db

--- controllers/household.ts
+++ controllers/household.ts
@@ -3,6 +3,16 @@ import {IHousehold} from "../models/household.model";
 
 const Household = require('../models/household.schema');
 
+exports.getAllHouseholds = (req: Request, res: Response, next: NextFunction) => {
+    Household.find()
+        .then((households: any) => {
+            res.send(households);
+        })
+        .catch((err: any) => {
+            console.log(err);
+        });
+};
+
 exports.createHousehold = (req: Request, res: Response, next: NextFunction) => {
     if (Object.keys(req.body).length === 0) {
         res.status(400).json({
--- routes/household.ts
+++ routes/household.ts
@@ -4,6 +4,7 @@ const householdController = require('../controllers/household');
 
 const router = express.Router();
 
+router.get('/', householdController.getAllHouseholds);
 router.post('/create', householdController.createHousehold);
 
 module.exports = router;
