d114fec8d674a95e4255e4fb17570ada374a7c61
Loh Chun Mun <chunmun_loh@live.com>
2022-08-09 18:33:26 +0800

    enable CORS

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
-import {Request, Response, NextFunction} from "express";
-import {insertSeed} from "./libs/mongodb.seed";
+import { Request, Response, NextFunction } from "express";
+import { insertSeed } from "./libs/mongodb.seed";
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const errorController = require('./controllers/error');
 
@@ -16,20 +17,24 @@ const app = express();
 
 /* Parsing the body of the request. */
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World!');
-});
+/* Allowing the server to accept requests from the client. */
+const corsOptions = {
+    methods: "GET, POST, PUT, DELETE, OPTIONS"
+}
+app.use(cors(corsOptions));
 
 /* Telling the app to use the householdRoutes file for all requests that start with /household. */
 app.use('/household', householdRoutes);
 
+/* A error controller that is called when the request is not handled by any other controllers. */
 app.use(errorController.get404);
 
 /* Connecting to the database and listening to port 3000. */
 mongoose.connect(MONGODB_URI)
     .then((result: any) => {
+        /* Calling the database seeding function. */
         insertSeed();
         app.listen(process.env.PORT);
     })
--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@ const mongodb_seed_1 = require("./libs/mongodb.seed");
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const errorController = require('./controllers/error');
 const householdRoutes = require('./routes/household');
 require('dotenv').config();
@@ -12,15 +13,19 @@ const app = express();
 /* Parsing the body of the request. */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
+/* Allowing the server to accept requests from the client. */
+const corsOptions = {
+    methods: "GET, POST, PUT, DELETE, OPTIONS"
+};
+app.use(cors(corsOptions));
 /* Telling the app to use the householdRoutes file for all requests that start with /household. */
 app.use('/household', householdRoutes);
+/* A error controller that is called when the request is not handled by any other controllers. */
 app.use(errorController.get404);
 /* Connecting to the database and listening to port 3000. */
 mongoose.connect(MONGODB_URI)
     .then((result) => {
+    /* Calling the database seeding function. */
     (0, mongodb_seed_1.insertSeed)();
     app.listen(process.env.PORT);
 })
